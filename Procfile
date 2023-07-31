edc7cbd65b67fbf292f38b189b18f7e5cb080bbf
Marc César Forte <99797041+marc-cesar@users.noreply.github.com>
2023-07-31 21:30:14 +0200

    flask modifications

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --bind 0.0.0.0:$PORT --chdir ML_Layer main:app
+web: gunicorn main:app
---
+++ main.py
@@ -0,0 +1,4 @@
+from ML_Layer.main.py import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
